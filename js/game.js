c47972dfd3496172a8b02d76155b398443609e25
Roan81 <110339856+Roan81@users.noreply.github.com>
2024-09-03 19:13:05 +0200

    add first class
    DA 05

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 let canvas;
 let ctx;
-let character = new Image ();
+let character = new MovableObject();
 
 
 function init(){
@@ -8,12 +8,7 @@ function init(){
     ctx = canvas.getContext('2d');
 
 
-    character.src = '../El-Pollo-Loco/img/2_character_pepe/1_idle/idle/I-1.png';
-
-    setTimeout(function(){
-        ctx.drawImage(character, 20, 20, 50, 150);
-
-    }, 2000);
+    console.log('My character is', character);
     
 
 }
