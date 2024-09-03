deda8f8d40b41d7cda6d9b832cfb69961603a46a
Roan81 <110339856+Roan81@users.noreply.github.com>
2024-09-03 23:50:59 +0200

    add character
    DA 09

--- models/world.class.js
+++ models/world.class.js
@@ -5,9 +5,13 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
-
+    ctx;
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
     draw() {
-        // Draw the world
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
     }
 }
