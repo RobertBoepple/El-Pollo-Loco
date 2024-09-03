4f4c19eb8753768381abbfa6213788cc23bd357d
Roan81 <110339856+Roan81@users.noreply.github.com>
2024-09-04 00:21:09 +0200

    add requestAnimationFrame
    DA 10

--- models/world.class.js
+++ models/world.class.js
@@ -12,6 +12,12 @@ class World {
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+          this.enemies.forEach(enemy => {
+                this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
+          });
+
+        requestAnimationFrame(() => this.draw());
     }
 }
