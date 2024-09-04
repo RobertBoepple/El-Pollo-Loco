6ebfc12f05a8ea8b231116857e4219f9a98e24ad
Roan81 <110339856+Roan81@users.noreply.github.com>
2024-09-04 12:41:41 +0200

    add placing enemy
    DA 11

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,6 +3,8 @@ class Chicken extends MovableObject {
 
     constructor(){
         super().loadImage('../El-Pollo-Loco/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 400 + Math.random() * 500;
     }
 
 }
