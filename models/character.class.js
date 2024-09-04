a73cbf47617bf4b60f4d546843064f9c799e3478
Roan81 <110339856+Roan81@users.noreply.github.com>
2024-09-04 22:30:48 +0200

    add animation character
    DA 2 03

--- models/character.class.js
+++ models/character.class.js
@@ -1,18 +1,33 @@
 class Character extends MovableObject{
     
-
-    constructor(){
-        super().loadImage('../El-Pollo-Loco/img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
+    IMAGES_WALKING = [
             '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-21.png',
             '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-22.png',
             '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-23.png',
             '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-24.png',
             '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-25.png',
             '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-26.png',
-            ]);
-    }
- 
+    ];
+    currentImage = 0;
 
-    jump(){}
+    constructor(){
+        super().loadImage('../El-Pollo-Loco/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+            this.animate();
+    }
+    
+    animate(){
+
+        setInterval(() => {
+          let i = this.currentImage % this.IMAGES_WALKING.length;  
+          let path = this.IMAGES_WALKING[i];
+          this.img = this.imageCache[path];
+          this.currentImage++;
+    }, 1000/7);
+    }
+
+    jump(){
+
+    }
 }
