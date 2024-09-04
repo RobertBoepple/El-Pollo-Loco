20f7bde7b28642eca329e2c25a2fb27ec1f0d5c9
Roan81 <110339856+Roan81@users.noreply.github.com>
2024-09-04 22:08:31 +0200

    add image cache character
    DA 2 02

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,14 @@ class Character extends MovableObject{
 
     constructor(){
         super().loadImage('../El-Pollo-Loco/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages([
+            '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-21.png',
+            '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-22.png',
+            '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-23.png',
+            '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-24.png',
+            '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-25.png',
+            '../El-Pollo-Loco/img/2_character_pepe/2_walk/W-26.png',
+            ]);
     }
  
 
